5794847d9debb0e1d6782ec499c31a23a812a3ed
Abdoo0000 <70906199+Abdoo0000@users.noreply.github.com>
2022-10-29 18:24:53 +0200

    Create SPYS-T-C.py

---
+++ SPYS-T-C.py
@@ -0,0 +1,44 @@
+import os
+import time
+import webbrowser
+os.system ("clear")
+print('''\033[35m
+           DIV : SPY0X & SPY1X
+          YOUTUBE : SPYS'TEAM
+	           ,.
+                 ,   `.
+               ,  _<>_ `.
+             , .- ____`-.`.
+           , _.-      ``-._`.
+         , ,       /\      `.`.
+       ,  /.._  O /  \ O  _.,\ `.
+     , / /  \ ``-;.--.:-   /  \ \`.
+   ,  : :    \  /\`., /\  /    : : `.
+  < <>||O >(< ( SPY'S TEAM )>)< O||<>>
+   `. : :    /  \/, `.\/  \    ; ; , 
+     `.\ \  /_..-:`-- ;-.._\  / /, 
+       `. \`    O \  / O   ` / , 
+         `.`._     \/     _, , 
+           `..``-.____.-  ,, 
+             `.`-.____.- , 
+               `.  <>  , 
+                 `.  ,  
+                   `
+ ''')
+print('''
+1 »  orange
+2 »  vodafone
+3 »  etesalat
+4 »  we
+              
+
+''')
+m = input("your chois: ")
+time.sleep(.9)
+print("\033[35m Plees whaite...")
+card = 1111111111111111
+while card > 0 :
+    print("\033[32m",card)
+    card=card +1322133121221321
+    time.sleep(.3)
+                  
